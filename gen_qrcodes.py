77fceac2526586c735adde962f4a80851ccb854e
DTeubl @xps <teubl.dani@gmail.com>
2025-04-10 14:40:08 +0200

    preview link added to intro slide

--- gen_qrcodes.py
+++ gen_qrcodes.py
@@ -8,3 +8,7 @@ acmu_repo.save("./figs/qr_acmu_repo.png")
 link = "https://github.com/dTeubl/acmu_refactoring_casestudy"
 slides = qr.make(link)
 slides.save("./figs/qr_slides.png")
+
+link = "https://htmlpreview.github.io/?https://github.com/dTeubl/acmu_refactoring_casestudy/blob/main/refactoring.html#"
+slides = qr.make(link)
+slides.save("./figs/preview.png")
